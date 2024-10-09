84fe15e1eac94ebb24f047f08d884ed0a0ccf71c
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-10-10 01:44:37 +0900

    リスト1-3-3までの動作を確認

--- React_sample_Yuma/my-modern/const.js
+++ React_sample_Yuma/my-modern/const.js
@@ -1,3 +1,11 @@
-const author = 'YAMADA.Yoshihiro';
-author = 'WINGS Project';
-console.log(author);
+// const author = 'YAMADA.Yoshihiro';
+// author = 'WINGS Project';
+// console.log(author);
+
+// const list = ['React', 'Vue', 'Angular'];
+// list[0] = 'Riot';
+// console.log(list);
+
+const list = ['React', 'Vue', 'Angular'];
+list = ['Java', 'C#', 'Python'];
+console.log(list);
--- React_sample_Yuma/my-modern/let.js
+++ React_sample_Yuma/my-modern/let.js
@@ -1,5 +1,9 @@
-if (true) {
-    let x = 13;
-}
-// ブロック配下で宣言した変数を参照すると…
-console.log(x);
+// if (true) {
+//     let x = 13;
+// }
+// // ブロック配下で宣言した変数を参照すると…
+// console.log(x);
+
+let msg = 'Hello!';
+let msg = 'Good Bye!';
+console.log(msg);
