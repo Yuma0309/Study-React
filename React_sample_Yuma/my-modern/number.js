56439aef4791fa4fb4d9946965cf6e40dcf2fab3
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-10-12 00:56:10 +0900

    リスト1-3-5までの動作を確認

--- React_sample_Yuma/my-modern/number.js
+++ React_sample_Yuma/my-modern/number.js
@@ -1,2 +1,14 @@
-const value = 123_456_789;
+// const value = 123_456_789;
+// console.log(value);
+
+// const value = 12_3_4_567;
+// console.log(value);
+
+// const value = 0._9876;
+// console.log(value);
+
+// const value = 9876_;
+// console.log(value);
+
+const value = 987__65;
 console.log(value);
