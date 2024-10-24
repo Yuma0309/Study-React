8053d8b3673c675cf3b2e80efd8a186f96c06b19
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-10-25 00:26:15 +0900

    リスト1-3-17までの動作を確認

--- React_sample_Yuma/my-modern/optional.js
+++ React_sample_Yuma/my-modern/optional.js
@@ -1,8 +1,15 @@
-const str = null;
-console.log(str.substring(1));
+// const str = null;
+// console.log(str.substring(1));
 
 // if (str !== null && str !== undefined) {
 //   console.log(str.substring(1));
 // }
 
 // console.log(str?.substring(1));
+
+// let value = null;
+// console.log(value ?? '既定値');
+
+let value = null;
+value ??= '既定値';
+console.log(value);
