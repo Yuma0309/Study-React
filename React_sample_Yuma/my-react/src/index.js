736d149b93985de286c55a4a818f76e8eb1de684
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-03 01:14:15 +0900

    リスト2-2-2までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,9 +1,13 @@
+// React関連のライブラリをインポート
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// アプリ固有のコードをインポート
 import './index.css';
 import App from './App';
+// パフォーマンス監視のためのサービスをインポート
 import reportWebVitals from './reportWebVitals';
 
+// Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,4 +18,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// パフォーマンス監視ツールを有効化
 reportWebVitals();
