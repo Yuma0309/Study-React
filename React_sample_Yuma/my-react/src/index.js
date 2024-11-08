ab047ae9eca6931ed309322b383477b1be893d5b
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-09 01:05:15 +0900

    リスト2-3-3までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom/client';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const name = '鈴木';
+// JSX式ではなく、ただの文字列リテラル
+const content = `<h3>WINGSプロジェクト</h3>
+  <img src="https://wings.msn.to/image/wings.jpg" />`;
 root.render(
-  <p>こんにちは、{name}です！</p>
+  // <p>{content}</p>
+  <p dangerouslySetInnerHTML={{__html: content}}></p>
 );
