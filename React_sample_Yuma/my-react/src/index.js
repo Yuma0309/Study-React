8f8acfd7e23bad776e822ffebae1c2eee0af3fe7
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-13 00:35:10 +0900

    リスト2-3-7までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const dest = 'https://ja.react.dev';
+
+// あまりよくない例
+const attrs = {
+  href: 'https://wings.msn.to/',
+  download: false,
+  target: '_blank',
+  rel: 'help'
+};
 
 root.render(
-  <a href={dest}>React本家サイト</a>
-  // <a href="{dest}">React本家サイト</a>
-  // <a href="{dest}/docs">React本家サイト</a>
-  // <a href={dest + '/docs'}>React本家サイト</a>
+  <a href={attrs.href} download={attrs.download}
+    target={attrs.target} rel={attrs.rel}>サポートページへ</a>
+  // <a {...attrs}>サポートページへ</a>
+  // <a href="index.html" download>トップへ</a>
+  // <a href="index.html" download={true}>トップへ</a>
 );
