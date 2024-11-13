95253d4ff60fa4584161d9cfc43cb17250061d86
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-14 00:01:39 +0900

    リスト2-3-8までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -4,18 +4,15 @@ import ReactDOM from 'react-dom/client';
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// あまりよくない例
-const attrs = {
-  href: 'https://wings.msn.to/',
-  download: false,
-  target: '_blank',
-  rel: 'help'
+// const props = 'color: White; background-color: Blue; padding: 3px';
+const props = {
+  color: 'White',
+  // backgroundColor: 'Blue',
+  'backgroundColor': 'Blue',
+  // padding: 3
+  padding: '3px'
 };
 
 root.render(
-  <a href={attrs.href} download={attrs.download}
-    target={attrs.target} rel={attrs.rel}>サポートページへ</a>
-  // <a {...attrs}>サポートページへ</a>
-  // <a href="index.html" download>トップへ</a>
-  // <a href="index.html" download={true}>トップへ</a>
+  <p style={props}>WINGSプロジェクト</p>
 );
