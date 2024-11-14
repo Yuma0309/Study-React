873d341e1b7602c1fc3b7a286941d39de8dc4c51
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-15 00:04:57 +0900

    リスト2-3-9までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,18 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
+import './chap02/class.css';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// const props = 'color: White; background-color: Blue; padding: 3px';
-const props = {
-  color: 'White',
-  // backgroundColor: 'Blue',
-  'backgroundColor': 'Blue',
-  // padding: 3
-  padding: '3px'
-};
-
 root.render(
-  <p style={props}>WINGSプロジェクト</p>
+  <p className="hoge">WINGSプロジェクト</p>
 );
