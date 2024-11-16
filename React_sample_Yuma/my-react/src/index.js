04975ea576764bd98ffbcc5ce6314f9207981db5
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-17 01:27:54 +0900

    リスト2-3-11までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,40 @@
 // React関連のライブラリをインポート
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './chap02/class.css';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const title = 'これからはじめるVue.js 3実践入門';
+
 root.render(
-  <p className="hoge">WINGSプロジェクト</p>
+  <div className="main">
+    <p>「{title}」（SBクリエイティブ刊）</p>
+    <img src="https://wings.msn.to/books/978-4-8156-1336-5/978-4-8156-1336-5.jpg"
+      alt={title} />
+    絶賛発売中！
+  </div>
+);
+
+root.render(
+  // 上位の<div>要素を生成
+  React.createElement(
+    'div',
+    { className: 'main' },
+    // 子要素の<p>、<img>、テキストを列挙
+    React.createElement(
+      'p',
+      null, // 属性は省略
+      `「${title}」（SBクリエイティブ刊）`
+    ),
+    React.createElement(
+      'img',
+      {
+        src: 'https://wings.msn.to/books/978-4-8156-1336-5/978-4-8156-1336-5.jpg',
+        alt: title
+      }
+    ),
+    '絶賛発売中！'
+  )
 );
