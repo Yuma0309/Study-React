7bdf13cd95a51bc0e20aae5c988cf5935a0b6b6e
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-20 01:48:31 +0900

    リスト3-1-3までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -6,5 +6,9 @@ import MyHello from './chap03/MyHello';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <MyHello myName="鈴木" />
+  // <MyHello myName="鈴木" />
+  <MyHello myName={13} />
+  // <MyHello myName={['山田', '鈴木', '佐藤']} />
+  // <MyHello myName={() => {console.log('Hoge'); }} />
+  // <MyHello myName={{ name: '鈴木', age: 48 }} />
 );
