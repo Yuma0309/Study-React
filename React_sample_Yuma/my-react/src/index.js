6b720e8f84586ed425f8ab63be663412ccd8e61a
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-22 00:52:24 +0900

    リスト3-1-5までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,14 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import MyHello from './chap03/MyHello';
+import EventBasic from './chap03/EventBasic';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // <MyHello myName="鈴木" />
-  <MyHello myName={13} />
-  // <MyHello myName={['山田', '鈴木', '佐藤']} />
-  // <MyHello myName={() => {console.log('Hoge'); }} />
-  // <MyHello myName={{ name: '鈴木', age: 48 }} />
+  <EventBasic type="time" />
 );
