73cb162b9e8045e2d2a58d2cb1d5e17169fef23d
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-24 01:23:04 +0900

    リスト3-1-7までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import EventBasic from './chap03/EventBasic';
+import StateBasic from './chap03/StateBasic';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <EventBasic type="time" />
+  <StateBasic init={0} />
 );
