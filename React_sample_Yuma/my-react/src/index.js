c9b09211cc4addd433e4f0a20c9a41a971ee6c2c
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-29 01:39:06 +0900

    リスト3-2-3までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,11 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import StateBasic from './chap03/StateBasic';
+import books from './chap03/books';
+import ForList from './chap03/ForList';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <StateBasic init={0} />
+  <ForList src={books} />
 );
