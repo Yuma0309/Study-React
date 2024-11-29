587d13e12d419569ea335d51dfb1cbdb042c70a3
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-11-30 01:50:39 +0900

    リスト3-2-4までの動作を確認

--- React_sample_Yuma/my-react/src/chap03/ForList.js
+++ React_sample_Yuma/my-react/src/chap03/ForList.js
@@ -6,14 +6,14 @@ export default function ForList({ src }) {
   // 書籍情報（src属性）を<dt>/<dd>リストに整形
   <dl>
     {src.map(elem => (
-      <>
+      <React.Fragment key={elem.isbn}>
         <dt>
           <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
               {elem.title}（{elem.price}円）
           </a>
         </dt>
         <dd>{elem.summary}</dd>
-      </>
+      </React.Fragment>
     ))}
   </dl>
   );
