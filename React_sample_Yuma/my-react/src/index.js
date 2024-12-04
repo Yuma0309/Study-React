de9f2b1afbd5c8021d1762e89ea6972e1e0291e9
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-05 00:23:17 +0900

    リスト3-2-9までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,11 +1,11 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
 import books from './chap03/books';
-import ForNest from './chap03/ForNest';
+import ForFilter from './chap03/ForFilter';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ForNest src={books} />
+  <ForFilter src={books} />
 );
