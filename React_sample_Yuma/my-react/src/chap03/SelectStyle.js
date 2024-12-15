0741a374340c43276b6572a5695e0510f18357c1
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-16 00:20:11 +0900

    リスト3-3-1までの動作を確認

--- React_sample_Yuma/my-react/src/chap03/SelectStyle.js
+++ React_sample_Yuma/my-react/src/chap03/SelectStyle.js
@@ -3,7 +3,26 @@ import cn from 'classnames';
 
 export default function SelectStyle({ mode }) {
   return (
-    <div className={cn('box', mode === 'light' ? 'light' : 'dark')}>
+    // <div className={cn('box', mode === 'light' ? 'light' : 'dark')}>
+    //   こんにちは、世界！
+    // </div>
+    // <div className={cn(
+    //   'box',
+    //   {
+    //     'light': mode === 'light',
+    //     'dark': mode === 'dark'
+    //   }
+    // )}>
+    <div className={cn(
+      'box',
+      [
+        'panel',
+        {
+          'light': mode === 'light',
+          'dark': mode === 'dark'
+        }
+      ]
+    )}>
       こんにちは、世界！
     </div>
   );
