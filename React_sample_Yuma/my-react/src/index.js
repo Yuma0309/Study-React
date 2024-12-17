59f43c6eb0630ba0f7c243b314402c2a8eb8e7d9
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-18 00:11:24 +0900

    リスト3-3-3までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,13 +1,15 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
 import StyledPanel from './chap03/StyledPanel';
+import MyHello from './chap03/MyHello';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <StyledPanel>
-    <p>メンバー募集中！</p>
-    <p>ようこそ、WINGSプロジェクトへ！！</p>
+    {/* <p>メンバー募集中！</p>
+    <p>ようこそ、WINGSプロジェクトへ！！</p> */}
+    <MyHello myName="鈴木" />
   </StyledPanel>
 );
