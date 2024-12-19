b3d793898f6e13ce6621756f61354b526af4d00a
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-20 01:02:15 +0900

    リスト3-3-5までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -4,15 +4,9 @@ import TitledPanel from './chap03/TitledPanel';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const title = <p>メンバー募集中！</p>;
+const body = <p>ようこそ、WINGSプロジェクトへ！！</p>;
 
 root.render(
-  <TitledPanel
-    title={
-      <p>メンバー募集中！</p>
-    }
-    body={
-      <p>ようこそ、WINGSプロジェクトへ！！</p>
-    }
-  >
-  </TitledPanel>
+  <TitledPanel title={title} body={body}></TitledPanel>
 );
