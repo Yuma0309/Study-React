9a24d993dd89333b0f45fac16fee3c184575b987
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-24 00:54:44 +0900

    リスト3-3-9までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,15 +1,18 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import TitledPanel from './chap03/TitledPanel';
+import books from './chap03/books';
+import ListTemplate from './chap03/ListTemplate';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const title = <p>メンバー募集中！</p>;
-const body = <p>ようこそ、WINGSプロジェクトへ！！</p>;
 
 root.render(
-  <TitledPanel>
-    <p key="title">メンバー募集中！</p>
-    <p key="body">ようこそ、WINGSプロジェクトへ！！</p>
-  </TitledPanel>
+  <ListTemplate src={books}>
+    <dt>
+      <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+        {elem.title} ({elem.price}円)
+      </a>
+    </dt>
+    <dd>{element.summary}</dd>
+  </ListTemplate>
 );
