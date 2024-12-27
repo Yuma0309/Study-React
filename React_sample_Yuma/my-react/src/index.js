471648290c4eef276212ffe09d342ec9f7e5ba64
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-28 01:40:48 +0900

    リスト3-3-13までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -7,16 +7,15 @@ import ListTemplate from './chap03/ListTemplate';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ListTemplate src={books}>
-    {elem => (
-      <>
-        <dt>
-          <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
-            {elem.title} ({elem.price}円)
-          </a>
-        </dt>
-        <dd>{elem.summary}</dd>
-      </>
-    )}
-  </ListTemplate>
+  // render属性として描画関数を渡す
+  <ListTemplate src={books} render={ elem => (
+    <>
+      <dt>
+        <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+          {elem.title} ({elem.price}円)
+        </a>
+      </dt>
+      <dd>{elem.summary}</dd>
+    </>
+  )}/>
 );
