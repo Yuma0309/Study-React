75c8fd80f873e16d9e17273557f3f067b53bb3c2
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-29 01:47:06 +0900

    リスト3-3-14までの動作を確認

--- React_sample_Yuma/my-react/src/chap03/MyHello.js
+++ React_sample_Yuma/my-react/src/chap03/MyHello.js
@@ -1,5 +1,15 @@
-export default function MyHello({ myName = '権兵衛'}) {
+// PropTypesをインポート
+import PropTypes from 'prop-types';
+
+function MyHello(props) {
   return (
-    <div>こんにちは、{myName}さん！</div>
+    <div>こんにちは、{props.myName}さん！</div>
   );
 }
+
+// 型情報を宣言
+MyHello.propTypes = {
+  myName: PropTypes.string.isRequired
+};
+
+export default MyHello;
