b69b183008bec6c8ca5d9498f9ffc128b2731714
中村祐馬 <yuma.nakamura.0309@gmail.com>
2024-12-30 02:53:39 +0900

    リスト3-3-15までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,21 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import books from './chap03/books';
-import ListTemplate from './chap03/ListTemplate';
+import MyHello from './chap03/MyHello';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // render属性として描画関数を渡す
-  <ListTemplate src={books} render={ elem => (
-    <>
-      <dt>
-        <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
-          {elem.title} ({elem.price}円)
-        </a>
-      </dt>
-      <dd>{elem.summary}</dd>
-    </>
-  )}/>
+  <MyHello />
 );
