35c9f57a966f637855d87a53f4766dfc3f8b63c2
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-01-05 01:50:18 +0900

    リスト3-3-21までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // 正しい例
-  // <TypeProp prop2="男性" />
+  // <TypeProp prop3="鈴木花子" />
   // 誤った例
-  <TypeProp prop2="hoge" />
+  <TypeProp prop3={new Member()} />
 );
