cc2b89f483ebca92259fec4e327ac9bc34b6a8a2
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-01-09 00:44:08 +0900

    リスト3-3-25までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // 正しい例
-  // <TypeProp prop4={[15, 30]} />
+  // <TypeProp prop5={{ '鈴木花子': 15, '佐藤雫': 30 }} />
   // 誤った例
-  <TypeProp prop4={[333, '鈴木花子']} />
+  <TypeProp prop5={{ '鈴木花子': 15, '佐藤雫': '三十' }} />
 );
