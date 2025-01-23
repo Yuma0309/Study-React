099c1bb3d28471c61e533d7b7a20c15648185b0f
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-01-24 00:14:14 +0900

    リスト3-4-9までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,11 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import EventError from './chap03/EventError';
+import EventObj from './chap03/EventObj';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <EventError src="./image/wings.jpg"
-    alt="サンプル画像" />
+  <EventObj />
 );
