0f5ee544975d20164b9486018f756338663a807b
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-02-07 00:34:33 +0900

    リスト3-4-21までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import EventArgs2 from './chap03/EventArgs2';
+import EventPropagation from './chap03/EventPropagation';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <EventArgs2 />
+  <EventPropagation />
 );
