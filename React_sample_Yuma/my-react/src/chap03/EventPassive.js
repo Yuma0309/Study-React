2d2ef8458595a838b4844360e2ea541998dff091
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-02-13 01:00:01 +0900

    リスト3-4-27までの動作を確認

--- React_sample_Yuma/my-react/src/chap03/EventPassive.js
+++ React_sample_Yuma/my-react/src/chap03/EventPassive.js
@@ -1,11 +1,22 @@
+import { useRef, useEffect } from 'react';
 import './css/EventPassive.css';
 
 export default function EventPassive() {
   const handleWheel = e => e.preventDefault();
+  // <div>要素への参照を取得
+  const divRef = useRef(null);
+  useEffect(() => {
+    // コンポーネント起動時にリスナーを設定
+    const div = divRef.current;
+    div.addEventListener('wheel', handleWheel, { passive: false });
+    return (() => {
+      // コンポーネント破棄時にリスナーも破棄
+      div.removeEventListener('wheel', handleWheel);
+    });
+  });
+
   return (
-    <div className="box"
-      onWheel={handleWheel}
-    >
+    <div ref={divRef} className="box" >
       たとえばwheelイベントをハンドラーで処理する状況を考えてみましょう。wheelイベントの既定の動作は、ページ／要素のスクロールです。
     </div>
   );
