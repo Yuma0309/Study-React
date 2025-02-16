a45cdef5b49cac3107c7b7d1355d366560ce7738
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-02-16 23:51:37 +0900

    リスト4-1-2までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import EventPassive from './chap03/EventPassive';
+import StateForm from './chap04/StateForm';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <EventPassive />
+  <StateForm />
 );
