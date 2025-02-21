f951626cd12457ed4c1fa97585f2d4fe97d1b8f9
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-02-22 06:09:31 +0900

    リスト4-1-8までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import FormTextarea from './chap04/FormTextarea';
+import FormSelect from './chap04/FormSelect';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <FormTextarea />
+  <FormSelect />
 );
