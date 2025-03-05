06e4466ef413915a766ff4f33063fb78102764ef
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-03-06 03:18:39 +0900

    リスト4-2-2までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import FormFile from './chap04/FormFile';
+import StateNest from './chap04/StateNest';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <FormFile />
+  <StateNest />
 );
