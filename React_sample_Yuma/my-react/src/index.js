70a3bead16c77fb4531a13a033372fe1186b1da1
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-03-10 09:00:31 +0900

    リスト4-2-6までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import StateNestImmer from './chap04/StateNestImmer';
+import StateNestImmer2 from './chap04/StateNestImmer2';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <StateNestImmer />
+  <StateNestImmer2 />
 );
