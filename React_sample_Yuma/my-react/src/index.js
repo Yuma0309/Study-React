54d972517bd1bca16dd622f82e1097138127be25
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-04-05 01:22:29 +0900

    リスト5-1-7までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import LazyMulti from './chap05/LazyMulti';
+import SuspenseSimple from './chap05/SuspenseSimple';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <LazyMulti />
+  <SuspenseSimple />
 );
