a9904e6eed5f33967e9b1dad6ea58d9077b95e06
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-04-10 09:15:51 +0900

    リスト5-1-12までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,10 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import SuspenseSimple from './chap05/SuspenseSimple';
+import SuspenseResult from './chap05/SuspenseResult';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <SuspenseSimple />
+  <SuspenseResult />
 );
