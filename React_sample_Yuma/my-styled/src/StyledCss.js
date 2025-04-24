c1c37ae3b0263a9ea3b7eafbc3c925bc4ce57481
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-04-24 09:25:10 +0900

    リスト5-2-10までの動作を確認

--- React_sample_Yuma/my-styled/src/StyledCss.js
+++ React_sample_Yuma/my-styled/src/StyledCss.js
@@ -1,11 +1,12 @@
-import css from './StyledCss.css';
+import css, { globalCss, resolveCss } from './StyledCss.css';
 
 export default function StyledCss() {
   return (
     <>
-    {/* 外部スタイルをインポート */}
     <style jsx>{css}</style>
-    <div className="panel"><b>Styled JSX</b>は、JSX式にスタイル定義を埋め込む形式のライブラリです。
+    <style jsx>{globalCss}</style>
+    {resolveCss.styles}
+    <div className={`panel ${resolveCss.className}`}><b>Styled JSX</b>は、JSX式にスタイル定義を埋め込む形式のライブラリです。
     </div>
     </>
   );
