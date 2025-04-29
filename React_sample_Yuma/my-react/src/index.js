ec0bc094c17b73314a64d9eb3874c756339bf611
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-04-30 00:07:42 +0900

    リスト5-2-16までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,13 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import StyledComp from './chap05/StyledComp';
+import { MyButton, MyStyledButton } from './chap05/StyledComp2';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <StyledComp />
+  <>
+    <MyButton>ボタン</MyButton>
+    <MyStyledButton>ボタン</MyStyledButton>
+  </>
 );
