d0622c5729d44e7e8dbea739fe111877b239053d
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-05-05 00:43:48 +0900

    リスト5-2-21までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,10 +1,14 @@
 // React関連のライブラリをインポート
 import ReactDOM from 'react-dom/client';
-import StyledCommon from './chap05/StyledCommon';
+import StyledComp from './chap05/StyledComp';
+import GlobalStyle from './chap05/StyledGlobal';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <StyledCommon />
+  <>
+    <GlobalStyle />
+    <StyledComp />
+  </>
 );
