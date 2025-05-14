29d0e7a36014dcb416b30326c11b474b331fc3d1
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-05-15 00:01:01 +0900

    リスト5-3-3までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,20 +1,12 @@
-/** @jsxImportSource @emotion/react */
-import { css, Global } from '@emotion/react';
 import ReactDOM from 'react-dom/client';
-import EmotionJsx from './chap05/EmotionJsx';
+import PortalBasic from './chap05/PortalBasic';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const global = css`
-  body {
-    background-color: Yellow;
-  }
-`;
-
 root.render(
   <>
-    <Global styles={global} />
-    <EmotionJsx />
+    <div id="dialog"></div>
+    <PortalBasic />
   </>
 );
