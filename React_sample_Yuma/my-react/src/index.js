0e35b31b50e2a39d021d2681a269f56797f0a0b5
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-05-18 00:04:12 +0900

    リスト5-3-6までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,12 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import PortalBasic from './chap05/PortalBasic';
+import ErrorRoot from './chap05/ErrorRoot';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <>
-    <div id="dialog"></div>
-    <PortalBasic />
-  </>
+  <ErrorRoot />
 );
