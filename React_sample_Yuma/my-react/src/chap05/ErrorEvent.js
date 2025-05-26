08e90cc3b81422257d161293c404c367da1283a1
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-05-27 01:03:51 +0900

    リスト6-1-1までの動作を確認

--- React_sample_Yuma/my-react/src/chap05/ErrorEvent.js
+++ React_sample_Yuma/my-react/src/chap05/ErrorEvent.js
@@ -1,6 +1,14 @@
+import { useErrorBoundary } from 'react-error-boundary';
+
 export default function ErrorEvent() {
+  const { showBoundary } = useErrorBoundary()
   const handleClick = () => {
-    throw new Error('Error is occured in MyApp.');
+    try {
+      throw new Error('Error is occured in MyApp.');
+    } catch (e) {
+      // ハンドラー内で発生した例外をError Boundaryに引き渡す
+      showBoundary(e);
+    }
   };
   return (
     <button type="button" onClick={handleClick}>
