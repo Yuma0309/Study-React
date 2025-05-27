c6d6f4ede50c0b745deb8241b2f09902bdbc5010
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-05-28 00:04:21 +0900

    リスト6-1-2までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import ErrorEventRoot from './chap05/ErrorEventRoot';
+import MaterialBasic from './chap06/MaterialBasic';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ErrorEventRoot />
+  <MaterialBasic />
 );
