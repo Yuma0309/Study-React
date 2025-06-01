f294a02fcb2117a2b04e64030df4d2b6e87be1c0
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-06-02 01:00:42 +0900

    リスト6-1-7までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import MaterialDrawer from './chap06/MaterialDrawer';
+import MaterialGrid from './chap06/MaterialGrid';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <MaterialDrawer />
+  <MaterialGrid />
 );
