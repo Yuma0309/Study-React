bf98012f150c76fb01408ffc590248040d08bd44
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-06-03 00:39:37 +0900

    リスト6-1-8までの動作を確認

--- React_sample_Yuma/my-react/src/chap06/MaterialGrid.js
+++ React_sample_Yuma/my-react/src/chap06/MaterialGrid.js
@@ -3,16 +3,16 @@ import { Button, Grid } from '@mui/material';
 export default function MaterialGrid() {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={6}>
+      <Grid item xs={12} sm={9} md={6}>
         <Button variant="contained" fullWidth>1</Button>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={12} sm={3} md={2}>
         <Button variant="contained" fullWidth>2</Button>
       </Grid>
-      <Grid item xs={3}>
+      <Grid item xs={12} sm={4} md={3}>
         <Button variant="contained" fullWidth>3</Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button variant="contained" fullWidth>4</Button>
       </Grid>
     </Grid>
