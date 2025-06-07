97b89149a159aa95d1e8517568518a2204067577
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-06-08 00:03:35 +0900

    リスト6-1-12までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './chap06/theme';
-import MaterialBasic from './chap06/MaterialBasic';
+import MaterialMode from './chap06/MaterialMode';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <MaterialBasic />
+    <MaterialMode />
   </ThemeProvider>
 );
