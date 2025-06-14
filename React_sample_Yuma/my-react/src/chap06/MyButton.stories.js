03da2f44618f0369bfd356716b6120cc172009df
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-06-15 02:16:17 +0900

    リスト6-2-4までの動作を確認

--- React_sample_Yuma/my-react/src/chap06/MyButton.stories.js
+++ React_sample_Yuma/my-react/src/chap06/MyButton.stories.js
@@ -8,11 +8,18 @@ export default {
 
 // Index、Whiteストーリーを追加
 export const Index = {
-  render: () => <MyButton primary size="medium" label="ボタン"
-  onClick={() => console.log('Hello, Storybook!!')}/>
+  args: {
+    primary: true,
+    size: 'medium',
+    label: 'ボタン',
+    onClick: () => console.log('Hello, Storybook!!')
+  }
 };
 
 export const White = {
-  render: () => <MyButton size="small" label="ボタン"
-    backgroundColor="#fff" />
+  args: {
+    size: 'small',
+    label: 'ボタン',
+    backgroundColor: '#fff',
+  }
 };
