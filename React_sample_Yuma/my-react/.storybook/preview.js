a4dc31a1b2a394ffcda6e89cebf1350739b36614
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-06-22 11:20:44 +0900

    リスト6-2-11までの動作を確認

--- React_sample_Yuma/my-react/.storybook/preview.js
+++ React_sample_Yuma/my-react/.storybook/preview.js
@@ -1,6 +1,6 @@
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: "^(on|handle)[A-Z].*" },
     controls: {
       matchers: {
        color: /(background|color)$/i,
