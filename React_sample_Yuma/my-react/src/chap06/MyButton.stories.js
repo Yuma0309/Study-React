fd142c6916ffa818670bf351bbe403e9d3f05c7b
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-06-24 00:27:38 +0900

    リスト6-2-14までの動作を確認

--- React_sample_Yuma/my-react/src/chap06/MyButton.stories.js
+++ React_sample_Yuma/my-react/src/chap06/MyButton.stories.js
@@ -1,21 +1,27 @@
+import { userEvent, within } from '@storybook/testing-library';
+import { expect, jest } from '@storybook/jest';
 import MyButton from './MyButton';
 
 // 基本情報を宣言
 export default {
   title: 'MyApp/MyButton',
   component: MyButton,
-  argTypes: {
-    handleClick: { action: 'clicked' },
-  },
 };
 
-// Index、Whiteストーリーを追加
+// Indexストーリーの動作を宣言
 export const Index = {
   args: {
     primary: true,
     size: 'medium',
     label: 'ボタン',
-    onClick: () => console.log('Hello, Storybook!!')
+    onClick: jest.fn(),
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button');
+    await userEvent.click(button);
+    await userEvent.click(button);
+    expect(args.onClick).toHaveBeenCalledTimes(2);
   }
 };
 
