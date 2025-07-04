9834bcc9193d1843a80cc85dcb9ac92fcb552ad4
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-07-04 09:21:30 +0900

    リスト6-3-5までの動作を確認

---
+++ React_sample_Yuma/my-react/src/chap06/QuerySuspense.js
@@ -0,0 +1,26 @@
+import { useQuery } from 'react-query';
+
+// delayミリ秒だけ処理を休止するsleep関数
+const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
+
+const fetchWeather = async () => {
+  // ダミーの遅延
+  await sleep(2000);
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Tokyo&lang=ja&appid=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`);
+  if (res.ok) { return res.json(); }
+  throw new Error(res.statusText);
+};
+
+export default function QuerySuspense() {
+  const { data } = useQuery('weather', fetchWeather);
+  // const { data } = useQuery('weather', fetchWeather, { suspense: true });
+
+  return (
+    <figure>
+      <img
+      src={`https://openweathermap.org/img/wn/${data?.weather?.[0]?.icon}.png`}
+      alt={data?.weather?.[0]?.main} />
+      <figcaption>{data?.weather?.[0]?.description}</figcaption>
+    </figure>
+  );
+}
