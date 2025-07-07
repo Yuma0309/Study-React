1c679eeab42303a8c4356d6da0eeb10197947f23
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-07-07 09:15:18 +0900

    リスト7-1-2までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,28 +1,9 @@
-import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import QueryBasic from './chap06/QueryBasic';
-import { ErrorBoundary } from 'react-error-boundary';
-import QuerySuspense from './chap06/QuerySuspense';
+import StateEffect from './chap07/StateEffect';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Suspenseモードを有効化
-const cli = new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true,
-    },
-  },
-});
-
 root.render(
-  <Suspense fallback={<p>Loading...</p>}>
-    <ErrorBoundary fallback={<div>エラーが発生しました。</div>}>
-      <QueryClientProvider client={cli}>
-        <QuerySuspense />
-      </QueryClientProvider>
-    </ErrorBoundary>
-  </Suspense>
+  <StateEffect init={0} />
 );
