efc171bee2e7a3e5ae7cf0d382e7762eeffa7008
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-07-22 01:00:32 +0900

    リスト7-2-10までの動作を確認

---
+++ React_sample_Yuma/my-react/src/chap07/HookCallbackRef.js
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from 'react';
+
+export default function HookCallbackRef() {
+  const [show, setShow] = useState(false);
+  // ボタンクリックで表示／非表示を反転
+  const handleClick = () => setShow(!show);
+  // [住所] 欄への参照
+  const address = useRef(null);
+  // [住所] 項目が空でなければフォーカスを移動
+  useEffect(() => {
+    if (address.current) {
+      address.current.focus();
+    }
+  }, [show]);
+
+  return (
+  <>
+  <div>
+      <label htmlFor="name">名前：</label>
+      <input id="name" type="text" />
+  </div>
+  <div>
+      <label htmlFor="email">メールアドレス：</label>
+      <input id="email" type="text" />
+      <button onClick={handleClick}>拡張表示</button>
+  {/* State (show) 値に応じて [住所] 欄を表示 */}
+  </div>
+  {show &&
+    <div>
+      <label htmlFor="address">住所：</label>
+      <input id="address" type="text" ref={address} />
+    </div>
+  }
+  </>
+  );
+}
