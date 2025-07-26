cac7ac7972fb8511cfa29c04dc74a6645b1f60ad
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-07-27 02:01:04 +0900

    リスト7-3-3までの動作を確認

---
+++ React_sample_Yuma/my-react/src/chap07/HookReducerUp.js
@@ -0,0 +1,35 @@
+import { useReducer } from 'react';
+
+export default function HookReducer({ init }) {
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        // カウント値の増減
+        case 'update':
+          return { count: state.count + action.step };
+        // カウント値の初期化
+        case 'reset' :
+            return { count: action.init };
+        // 既定の挙動（Stateをそのまま返す）
+        default:
+          return state;
+      }
+    },
+    {
+      count: init
+    }
+  );
+  // それぞれのボタンに対応したハンドラー
+  const handleUp = () => dispatch({ type: 'update', step: 1 });
+  const handleDown = () => dispatch({ type: 'update', step: -1 });
+  const handleReset = () => dispatch({ type: 'reset', init });
+
+  return (
+    <>
+      <button onClick={handleUp}>カウントアップ</button>
+      <button onClick={handleDown}>カウントダウン</button>
+      <button onClick={handleReset}>リセット</button>
+      <p>{state.count}回、クリックされました。</p>
+    </>
+  );
+}
