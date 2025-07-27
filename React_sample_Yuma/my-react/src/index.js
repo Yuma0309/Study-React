f3c9c3ae69dc2871faeba652afec83719f614826
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-07-28 00:10:02 +0900

    リスト7-3-4までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import HookReducer from './chap07/HookReducer';
+import HookReducerUp from './chap07/HookReducerUp';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <HookReducer init={0} />
+  <HookReducerUp init={0} />
 );
