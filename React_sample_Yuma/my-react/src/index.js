8b194be1d468ef6e5622411199ec64ad76acb542
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-08-05 00:06:48 +0900

    リスト7-4-6までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import HookContext from './chap07/HookContext';
+import MyThemeProvider from './chap07/MyThemeProvider';
+import HookThemeButton from './chap07/HookThemeButton';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <HookContext />
+  <MyThemeProvider>
+    <HookThemeButton />
+  </MyThemeProvider>
 );
