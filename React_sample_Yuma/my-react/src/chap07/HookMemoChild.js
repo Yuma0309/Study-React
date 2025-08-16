33648b97b38177b661fde1403c9e98120bd4374d
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-08-17 00:17:50 +0900

    リスト7-6-2までの動作を確認

---
+++ React_sample_Yuma/my-react/src/chap07/HookMemoChild.js
@@ -0,0 +1,19 @@
+// カウンターを増減するためのボタン
+// id：id値、handleClick：ボタンのハンドラー、children：ボタンキャプション
+export const MyButton = ({ id, handleClick, children }) => {
+  // 再描画時にログ
+  console.log(`MyButton is called: ${id}`);
+  return (
+    <button onClick={handleClick}>{children}</button>
+  );
+};
+
+// カウンター値を表示するためのラベル
+// id：id値、value：表示する値
+export const MyCounter = ({ id, value }) => {
+  // 再描画時にログ
+  console.log(`MyCounter is called: ${id}`);
+  return (
+    <p>現在値：{value}</p>
+  );
+};
