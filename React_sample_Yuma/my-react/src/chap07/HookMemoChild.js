ebf36782215c66691731e764985c2c26d0e3d671
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-08-20 00:20:35 +0900

    リスト7-6-5までの動作を確認

--- React_sample_Yuma/my-react/src/chap07/HookMemoChild.js
+++ React_sample_Yuma/my-react/src/chap07/HookMemoChild.js
@@ -1,19 +1,21 @@
+import { memo } from 'react';
+
 // カウンターを増減するためのボタン
 // id：id値、handleClick：ボタンのハンドラー、children：ボタンキャプション
-export const MyButton = ({ id, handleClick, children }) => {
+export const MyButton = memo(({ id, handleClick, children }) => {
   // 再描画時にログ
   console.log(`MyButton is called: ${id}`);
   return (
     <button onClick={handleClick}>{children}</button>
   );
-};
+});
 
 // カウンター値を表示するためのラベル
 // id：id値、value：表示する値
-export const MyCounter = ({ id, value }) => {
+export const MyCounter = memo(({ id, value }) => {
   // 再描画時にログ
   console.log(`MyCounter is called: ${id}`);
   return (
     <p>現在値：{value}</p>
   );
-};
+});
