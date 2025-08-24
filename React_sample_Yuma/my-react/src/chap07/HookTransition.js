7b828cb0449a287296e07254e9cea072ae974db6
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-08-25 00:26:55 +0900

    リスト7-7-4までの動作を確認

--- React_sample_Yuma/my-react/src/chap07/HookTransition.js
+++ React_sample_Yuma/my-react/src/chap07/HookTransition.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import books from './books';
 import commentList from './comments';
 import { BookDetails, CommentList } from './HookTransitionChild';
@@ -7,12 +7,18 @@ export default function HookTransition() {
   // 選択された書籍（isbn）と対応するコメント（comments）
   const [isbn, setIsbn] = useState('');
   const [comments, setComments] = useState([]);
+  // トランジションを利用するための準備
+  const [isPending, startTransition] = useTransition();
+
   // 選択ボックスの変更に応じてStateを反映
   const handleChange = e => {
     const isbn = e.target.value;
     setIsbn(isbn);
-    setComments(commentList.filter(c => c.isbn === isbn));
-  };
+    // トランジションの配下でStateを更新する
+    startTransition(() => {
+      setComments(commentList.filter(c => c.isbn === isbn));
+    });
+  }
 
   return (
     <>
