33f45743196e0ae4d43daf224bcc1a8da9c06eca
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-08-29 00:08:08 +0900

    リスト7-7-8までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import HookTransition from './chap07/HookTransition';
+import HookDefered from './chap07/HookDeferred';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <HookTransition />
+  <HookDefered />
 );
