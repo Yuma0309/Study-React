dbece58ef0b35acca92edd424e59deec451b41e4
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-09-07 02:55:46 +0900

    リスト8-1-3までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/routesBasic.js
+++ React_sample_Yuma/my-react/src/chap08/routesBasic.js
@@ -1,15 +1,19 @@
 // React Router本体をインポート
-import { createBrowserRouter } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 // ルーティングで利用するページをインポート
 import TopPage from './TopPage';
 import ArticlePage from './ArticlePage';
 import AboutPage from './AboutPage';
 
 // ルーティングテーブルを定義
-const routesBasic = createBrowserRouter([
-  { path: '/', element: <TopPage /> },
-  { path: '/article', element: <ArticlePage /> },
-  { path: '/about', element: <AboutPage /> },
-]);
+const routesBasic = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<TopPage />} />
+      <Route path="/article" element={<ArticlePage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </>
+  )
+);
 
 export default routesBasic;
