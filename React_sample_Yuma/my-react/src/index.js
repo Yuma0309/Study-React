58b3ca0d5fef1401d318bb5b14e80b1e4741b2a6
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-09-10 00:55:53 +0900

    リスト8-2-3までの動作を確認

--- React_sample_Yuma/my-react/src/index.js
+++ React_sample_Yuma/my-react/src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import routerBasic from './chap08/routesBasic';
+import routersLink from './chap08/routersLink';
 
 // Reactアプリ（Appコンポーネント）を描画
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // React Routerを有効化
 root.render(
-  <RouterProvider router={routerBasic} />
+  <RouterProvider router={routersLink} />
 );
