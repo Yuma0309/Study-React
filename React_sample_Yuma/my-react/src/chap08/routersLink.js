ab48ce07bf64c5f73a23da41783cef02143afc50
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-09-14 05:53:13 +0900

    リスト8-2-7までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/routersLink.js
+++ React_sample_Yuma/my-react/src/chap08/routersLink.js
@@ -1,12 +1,12 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import RouterApp from './RouterApp';
+import RouterNav from './RouterNav';
 import TopPage from './TopPage';
 import ArticlePage from './ArticlePage';
 import AboutPage from './AboutPage';
 
 const routesLink = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RouterApp />}>
+    <Route element={<RouterNav />}>
       <Route path="/" element={<TopPage />} />
       <Route path="/article" element={<ArticlePage />} />
       <Route path="/about" element={<AboutPage />} />
