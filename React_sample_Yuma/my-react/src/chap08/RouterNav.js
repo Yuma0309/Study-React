551de96d93f6e5f2f449e5e7bfb1ca0adcc36e55
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-09-16 00:29:16 +0900

    リスト8-2-9までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/RouterNav.js
+++ React_sample_Yuma/my-react/src/chap08/RouterNav.js
@@ -1,15 +1,18 @@
 import { NavLink, Outlet } from 'react-router-dom';
 import './RouterNav.css';
 
-const isCurrent = ({ isActive, isPending }) => isActive ? 'active' : '';
+const isCurrent = ({ isActive }) => isActive ? {
+  color: 'Red',
+  fontWeight: 'bold'
+} : {};
 
 export default function RouterNav() {
   return (
     <>
       <ul>
-        <li><NavLink className={isCurrent} to="/">トップ</NavLink></li>
-        <li><NavLink className={isCurrent} to="/article">公開記事</NavLink></li>
-        <li><NavLink className={isCurrent} to="/about">このサイトについて</NavLink></li>
+        <li><NavLink style={isCurrent} to="/">トップ</NavLink></li>
+        <li><NavLink style={isCurrent} to="/article">公開記事</NavLink></li>
+        <li><NavLink style={isCurrent} to="/about">このサイトについて</NavLink></li>
       </ul>
       <hr />
       <Outlet />
