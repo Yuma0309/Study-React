93ff1962c7f51c73b4e6d7cbc46b4607fe16289e
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-09-18 01:18:39 +0900

    リスト8-2-11までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/RouterApp.js
+++ React_sample_Yuma/my-react/src/chap08/RouterApp.js
@@ -8,6 +8,7 @@ export default function RouterApp() {
         <li><Link to="/">トップ</Link></li>
         <li><Link to="/article">公開記事</Link></li>
         <li><Link to="/about" replace>このサイトについて</Link></li>
+        <li><Link to ="/article/info">Infoページ</Link></li>
       </ul>
       <hr />
       {/* リンク先の表示領域を確保 */}
