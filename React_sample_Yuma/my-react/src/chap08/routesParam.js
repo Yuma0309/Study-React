c38dbf49760aff8f8f201ec8969620d4794cddc7
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-10-03 01:01:53 +0900

    リスト8-3-11までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/routesParam.js
+++ React_sample_Yuma/my-react/src/chap08/routesParam.js
@@ -3,6 +3,7 @@ import RouterParam from './RouterParam';
 import TopPage from './TopPage';
 import BookPage from './BookPage';
 import SearchPage from './SearchPage';
+import NotFoundPage from './NotFoundPage';
 
 const routesParam = createBrowserRouter(
   createRoutesFromElements(
@@ -11,6 +12,8 @@ const routesParam = createBrowserRouter(
       <Route path="/book/:isbn?" element={<BookPage />} />
       {/* 可変長パラメーターを定義 */}
       <Route path="/search/*" element={<SearchPage />} />
+      {/* 任意のページに対応するルート */}
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
