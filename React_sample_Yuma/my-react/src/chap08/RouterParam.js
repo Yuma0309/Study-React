c5ef973ecc47e7d879ac0de1566dca6ca749064f
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-10-05 00:04:41 +0900

    リスト8-3-13までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/RouterParam.js
+++ React_sample_Yuma/my-react/src/chap08/RouterParam.js
@@ -12,6 +12,7 @@ export default function RouterParam() {
           改訂3版 JavaScript本格入門</NavLink></li>
         <li><NavLink to="/book" end>既定の書籍</NavLink></li>
         <li><NavLink to="/search/react/router/remix">検索結果</NavLink></li>
+        <li><NavLink to="/nothing/foo/bar">存在しないページ</NavLink></li>
       </ul>
       <hr />
       <Outlet />
