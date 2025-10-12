a690139fe17605b4e833466458bf167ddb02de2e
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-10-13 00:33:13 +0900

    リスト8-3-21までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/TopPage.js
+++ React_sample_Yuma/my-react/src/chap08/TopPage.js
@@ -1,16 +1,8 @@
-// import { useEffect } from 'react';
-// import { useOutletContext } from 'react-router-dom';
-// import MyHeader from './MyHeader';
+import { useEffect } from 'react';
+import { useOutletContext } from 'react-router-dom';
 
 export default function TopPage() {
-  // const [, setCount] = useOutletContext();
-  // useEffect(() => setCount(c => c + 1), [setCount]);
+  const [, setCount] = useOutletContext();
+  useEffect(() => setCount(c => c + 1), [setCount]);
   return <p>トップページです。</p>;
-
-  // return (
-  //   <>
-  //     <MyHeader />
-  //     <p>トップページです。</p>
-  //   </>
-  // );
 }
