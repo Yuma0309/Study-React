89ee2de6922db3e16bea7a93ad84eab1e978dc6a
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-10-18 01:45:38 +0900

    リスト8-3-26までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/BookPage.js
+++ React_sample_Yuma/my-react/src/chap08/BookPage.js
@@ -1,7 +1,16 @@
 import { useParams } from 'react-router-dom';
+import MyHeader from './MyHeader';
+import books from '../chap07/books';
 
 export default function BookPage() {
-  // ルートパラメーターを取得
   const { isbn = '978-4-8156-0182-9' } = useParams();
-  return <p>ISBNコード「{isbn}」のページです。</p>;
+  const { title, summary } = books.find(b => isbn === b.isbn);
+
+  return (
+    <>
+    <MyHeader title={title} keywords={title}
+      description={summary} />
+    <p>ISBNコード「{isbn}」のページです。</p>
+    </>
+  );
 }
