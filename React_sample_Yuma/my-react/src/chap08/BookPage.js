9680a123cd4a8d1d56ed9e78015271526b47e108
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-10-19 03:28:25 +0900

    リスト8-4-1までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/BookPage.js
+++ React_sample_Yuma/my-react/src/chap08/BookPage.js
@@ -1,16 +1,10 @@
 import { useParams } from 'react-router-dom';
-import MyHeader from './MyHeader';
-import books from '../chap07/books';
 
 export default function BookPage() {
   const { isbn = '978-4-8156-0182-9' } = useParams();
-  const { title, summary } = books.find(b => isbn === b.isbn);
-
-  return (
-    <>
-    <MyHeader title={title} keywords={title}
-      description={summary} />
-    <p>ISBNコード「{isbn}」のページです。</p>
-    </>
-  );
+  // ISBNコードの形式に則っていなければエラー
+  if (!/978-4-[0-9]{2,7}-[0-9]{2,7}-[0-9X]/.test(isbn)) {
+    throw new RangeError('ISBN is invalid!!');
+  }
+  return <p>ISBNコード「{isbn}」のページです。</p>
 }
