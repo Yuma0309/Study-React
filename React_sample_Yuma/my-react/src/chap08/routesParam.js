3b9c1f1ef4f379dfd4e38483fb710ffb952c1792
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-10-23 23:57:50 +0900

    リスト8-4-5までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/routesParam.js
+++ React_sample_Yuma/my-react/src/chap08/routesParam.js
@@ -7,17 +7,23 @@ import NotFoundPage from './NotFoundPage';
 import BookQueryPage from './BookQueryPage';
 import BookStatePage from './BookStatePage';
 import InvalidParamsPage from './InvalidParamsPage';
+import WeatherPage from './chap08/WeatherPage';
 
 const routesParam = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RouterParam />}
-      errorElement={<InvalidParamsPage />}>
+    <Route element={<RouterParam />}>
       <Route path="/" element={<TopPage />} />
-      <Route path="/book/:isbn?" element={<BookPage />} />
+      <Route path="/book/:isbn?" element={<BookPage />}
+        errorElement={<InvalidParamsPage />} />
       <Route path="/bookQuery" element={<BookQueryPage />} />
       <Route path="/bookState" element={<BookStatePage />} />
       {/* 可変長パラメーターを定義 */}
       <Route path="/search/*" element={<SearchPage />} />
+      <Route path="/weather/:city" element={<WeatherPage />}
+        loader={({ params }) =>
+          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=ja&appid=a3efa5816300843820fe3f35e9540b32`)
+        }
+        />
       {/* 任意のページに対応するルート */}
       <Route path="*" element={<NotFoundPage />} />
     </Route>
