d879f115f342bd52d6f7dcff4012c5932f731bbe
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-10-26 00:30:14 +0900

    リスト8-4-8までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/routesParam.js
+++ React_sample_Yuma/my-react/src/chap08/routesParam.js
@@ -1,4 +1,4 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, json, Route } from 'react-router-dom';
 import RouterParam from './RouterParam';
 import TopPage from './TopPage';
 import BookPage from './BookPage';
@@ -8,6 +8,19 @@ import BookQueryPage from './BookQueryPage';
 import BookStatePage from './BookStatePage';
 import InvalidParamsPage from './InvalidParamsPage';
 import WeatherPage from './WeatherPage';
+
+const fetchWeather = async ({ params }) => {
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=ja&appid=a3efa5816300843820fe3f35e9540b32`);
+  // 応答が成功の場合は、そのまま結果データを返す
+  if (res.ok) { return res; }
+  // 成功以外の結果ではエラーデータを生成
+  return json({
+    "weather":[
+      {"id":803,"main":"Unknown","description":"不明","icon":"50d"}
+    ],
+    "name":"不明"
+  });
+}
 
 const routesParam = createBrowserRouter(
   createRoutesFromElements(
@@ -20,10 +33,7 @@ const routesParam = createBrowserRouter(
       {/* 可変長パラメーターを定義 */}
       <Route path="/search/*" element={<SearchPage />} />
       <Route path="/weather/:city" element={<WeatherPage />}
-        loader={({ params }) =>
-          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=ja&appid=a3efa5816300843820fe3f35e9540b32`)
-        }
-        />
+        loader={fetchWeather} />
       {/* 任意のページに対応するルート */}
       <Route path="*" element={<NotFoundPage />} />
     </Route>
