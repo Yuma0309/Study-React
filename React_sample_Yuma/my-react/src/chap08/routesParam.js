41dfbed1a15fe51b4d5ac6df5a503669e25acd89
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-10-29 00:01:07 +0900

    リスト8-4-11までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/routesParam.js
+++ React_sample_Yuma/my-react/src/chap08/routesParam.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, json, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import RouterParam from './RouterParam';
 import TopPage from './TopPage';
 import BookPage from './BookPage';
@@ -8,6 +8,7 @@ import BookQueryPage from './BookQueryPage';
 import BookStatePage from './BookStatePage';
 import InvalidParamsPage from './InvalidParamsPage';
 import WeatherPage from './WeatherPage';
+import CommonErrorPage from './CommonErrorPage';
 
 const fetchWeather = async ({ params }) => {
   const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=ja&appid=a3efa5816300843820fe3f35e9540b32`);
@@ -15,17 +16,18 @@ const fetchWeather = async ({ params }) => {
   // レスポンスステータスに応じて、異なるエラー情報をスロー
   switch (res.status) {
     case 404:
-      throw json({ message: 'city is invalid!!' }, { status: 404 });
+      throw new Response(JSON.stringify({ message: 'city is invalid!!' }), { status: 404 });
     case 401:
-      throw json({ message: 'api key is invalid!!' }, { status: 401 });
+      throw new Response(JSON.stringify({ message: 'api key is invalid!!' }), { status: 401 });
     default:
-      throw json({ message: 'api server is in trouble...' }, { status: 501 });
+      throw new Response(JSON.stringify({ message: 'api server is in trouble...' }), { status: 501 });
   }
 }
 
 const routesParam = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RouterParam />}>
+    <Route element={<RouterParam />}
+      errorElement={<CommonErrorPage />}>
       <Route path="/" element={<TopPage />} />
       <Route path="/book/:isbn?" element={<BookPage />}
         errorElement={<InvalidParamsPage />} />
