9ed671fb31a543426971e67bd6e5a0afb673c9f3
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-11-03 04:08:36 +0900

    リスト8-4-16までの動作を確認

--- React_sample_Yuma/my-react/src/chap08/BookFormPage.js
+++ React_sample_Yuma/my-react/src/chap08/BookFormPage.js
@@ -1,8 +1,13 @@
-import { Form } from 'react-router-dom';
+import { Form, useActionData } from 'react-router-dom';
 
 export default function BookFormPage() {
+  const errors = useActionData();
+
   return (
   <Form method="POST" noValidate>
+    <ul>
+    {errors?.map(msg => <li key={msg}>{msg}</li>)}
+    </ul>
     <div>
       <label htmlFor="title">書名：</label><br/>
       <input id="title" name="title" type="text" size="20" />
