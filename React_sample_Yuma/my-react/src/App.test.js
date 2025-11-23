83c026982252bbaf34304847fe46d6baae569eb9
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-11-23 11:25:59 +0900

    リスト9-1-3までの動作を確認

--- React_sample_Yuma/my-react/src/App.test.js
+++ React_sample_Yuma/my-react/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// テストケースを定義
 test('renders learn react link', () => {
+  // コンポーネントを描画
   render(<App />);
+  // テスト対象の要素を検索&取得
   const linkElement = screen.getByText(/learn react/i);
+  // 描画結果が正しいかを検証
   expect(linkElement).toBeInTheDocument();
 });
