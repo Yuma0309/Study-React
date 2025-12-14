b4a3a3480f676d48018e8d87127959fd811729c2
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-12-14 13:59:20 +0900

    リスト10-2-7までの動作を確認

--- React_sample_Yuma/my-react-ts/src/index.tsx
+++ React_sample_Yuma/my-react-ts/src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ListTemplate from './ListTemplate';
+import books from './books';
+import type { Book } from './Book';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ListTemplate src={books}>
+  {(elem: Book) => (
+    <>
+    <dt>
+      <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+          {elem.title}（{elem.price}円）
+      </a>
+    </dt>
+    <dd>{elem.summary}</dd>
+    </>
+  )}
+  </ListTemplate>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
