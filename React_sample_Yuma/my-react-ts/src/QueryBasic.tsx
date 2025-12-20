d7c16778b001a077d21ea793651851f77a06907d
中村祐馬 <yuma.nakamura.0309@gmail.com>
2025-12-21 01:55:25 +0900

    リスト10-2-15までの動作を確認

---
+++ React_sample_Yuma/my-react-ts/src/QueryBasic.tsx
@@ -0,0 +1,42 @@
+import { useQuery } from '@tanstack/react-query';
+
+type WeatherType = {
+  weather: Array<{
+    id: number
+    main: string
+    description: string
+    icon: string
+  }>
+}
+
+// 天気情報を取得するための関数
+const fetchWeather = async () => {
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Tokyo&lang=ja&appid=ef23a61e94f59784b8451d12c0d07da8`);
+  if (res.ok) {
+    return  await res.json() as WeatherType;
+  }
+  throw new Error(res.statusText);
+};
+
+export default function QuerBasic() {
+  // fetchWeather関数でデータを取得
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['weather'],
+    queryFn: fetchWeather
+  });
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+  if (isError) {
+    return <p>Error: {error.message}</p>
+  }
+
+  return (
+    <figure>
+      <img
+      src={`https://openweathermap.org/img/wn/${data?.weather?.[0]?.icon}.png`}
+      alt={data?.weather?.[0]?.main} />
+      <figcaption>{data?.weather?.[0]?.description}</figcaption>
+    </figure>
+  );
+}
--- React_sample_Yuma/my-react-ts/src/index.tsx
+++ React_sample_Yuma/my-react-ts/src/index.tsx
@@ -1,22 +1,13 @@
 import ReactDOM from 'react-dom/client';
-import ListTemplate from './ListTemplate';
-import books from './books';
-import type { Book } from './Book';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import QuerBasic from './QueryBasic';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ListTemplate src={books}>
-  {(elem: Book) => (
-    <>
-    <dt>
-      <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
-          {elem.title}（{elem.price}円）
-      </a>
-    </dt>
-    <dd>{elem.summary}</dd>
-    </>
-  )}
-  </ListTemplate>
+  <QueryClientProvider client={queryClient}>
+    <QuerBasic />
+  </QueryClientProvider>
 );
