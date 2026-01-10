e8a72add1ab39730cb2773990689c80a910ef30f
中村祐馬 <yuma.nakamura.0309@gmail.com>
2026-01-11 04:23:29 +0900

    リスト11-4-12までの動作を確認

--- React_sample_Yuma/my-next/src/app/edit/[id]/page.js
+++ React_sample_Yuma/my-next/src/app/edit/[id]/page.js
@@ -1,6 +1,19 @@
-import { use } from 'react';
+import BookDetails from '@/components/BookDetails';
+import FormEdit from '@/components/FormEdit';
+import { getBookById, getReviewById } from '@/lib/getter';
 
-export default function EditPage({ params }) {
-  const p = use(params);
-  return <p>No. {p.id}のレビューを表示しています。</p>;
+export default async function EditPage({ params }) {
+  const book = await getBookById(params.id);
+  const review = await getReviewById(params.id);
+  // 「YYYY-MM-DD」形式の日付を生成
+  const read = (review?.read || new Date()).toLocaleDateString('sv-SE');
+
+  return (
+    <div id="form">
+      <BookDetails book={book} />
+      <hr />
+      {/* 編集フォームを生成 */}
+      <FormEdit src={{ id: book.id, read, memo: review?.memo }} />
+    </div>
+  );
 }
