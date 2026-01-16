7596f4bfd74e66be48c23dab147ec12e5de87f02
中村祐馬 <yuma.nakamura.0309@gmail.com>
2026-01-17 01:00:39 +0900

    Prisma設定の修正とDB接続エラーの解消（v7→v5へダウングレード）

--- React_sample_Yuma/my-next/src/lib/prisma.js
+++ React_sample_Yuma/my-next/src/lib/prisma.js
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 // global.prisma上にPrismaクライアントが存在すれば再利用
 const prisma = global.prisma ??
   new PrismaClient({ log: ['query'] });
-  // 非Production環境ではglobal.prismaにオブジェクトを格納
+// 非Production環境ではglobal.prismaにオブジェクトを格納
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default prisma;
