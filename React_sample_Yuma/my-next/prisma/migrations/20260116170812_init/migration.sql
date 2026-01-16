f6b093362ad014e57318574eed3ec8e57849fd80
中村祐馬 <yuma.nakamura.0309@gmail.com>
2026-01-17 02:12:54 +0900

    リスト11-5-1までの動作を確認

--- React_sample_Yuma/my-next/prisma/migrations/20260116170812_init/migration.sql
+++ React_sample_Yuma/my-next/prisma/migrations/20260116170812_init/migration.sql
@@ -1,12 +1,14 @@
 -- CreateTable
 CREATE TABLE "reviews" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "author" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
     "publisher" TEXT NOT NULL,
     "published" TEXT NOT NULL,
     "image" TEXT NOT NULL,
-    "read" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "memo" TEXT NOT NULL
+    "read" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "memo" TEXT NOT NULL,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
 );
